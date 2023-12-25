bf96426bd9e8c7d4f99674ce7cea0e808b3e573b
ljunker <larsjunker92@gmail.com>
2023-12-25 22:50:04 +0100

    euler 3

---
+++ euler-0003.py
@@ -0,0 +1,19 @@
+import math
+
+
+def prime_factors(n):
+    while n % 2 == 0:
+        yield 2
+        n = n / 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            yield i
+            n = n / i
+
+    if n > 2:
+        yield n
+
+
+n = 600851475143
+print(max([x for x in prime_factors(n)]))
