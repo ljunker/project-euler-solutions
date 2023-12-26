3ed48b8b5950ef8c3593e8c1f6956cfb61e48ee2
ljunker <larsjunker92@gmail.com>
2023-12-26 21:32:44 +0100

    euler 7

---
+++ euler-0007.py
@@ -0,0 +1,26 @@
+from math import ceil, log
+
+
+def find_primes(limit):
+    nums = [True] * (limit + 1)
+    nums[0] = nums[1] = False
+
+    for (i, is_prime) in enumerate(nums):
+        if is_prime:
+            yield i
+            for n in range(i * i, limit + 1, i):
+                nums[n] = False
+
+
+def upper_bound(n):
+    if n < 6:
+        return 100
+    return ceil(n * (log(n) + log(log(n))))
+
+
+def find_n_prime(n):
+    primes = list(find_primes(upper_bound(n)))
+    return primes[n - 1]
+
+
+print(find_n_prime(10001))
